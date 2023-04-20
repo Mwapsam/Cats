0d3d154b05f0b7f11750187e3fa0831cd22234a8
Mwapsam <mwapsam@gmail.com>
2023-04-20 09:39:14 +0200

    Create Bread info component

---
+++ src/components/HomePage/BreedInfo.tsx
@@ -0,0 +1,27 @@
+import { useCatContext } from '../../context/CatContext';
+import { Card } from 'react-bootstrap';
+
+type Props = {
+    cats: any[]
+}
+
+const BreedInfo = ({cats}: Props ) => {
+  const { selectedBreed } = useCatContext();
+  const selectedCat = cats.find((cat) => cat.breeds[0].id === selectedBreed);
+
+  if (!selectedCat) {
+    return null;
+  }
+
+  return (
+    <Card style={{width: '14rem'}}>
+      <Card.Header>{selectedBreed}</Card.Header>
+      <Card.Body>
+        <Card.Title>{selectedCat.breeds[0].name}</Card.Title>
+        <Card.Text>{selectedCat.breeds[0].description}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default BreedInfo;
